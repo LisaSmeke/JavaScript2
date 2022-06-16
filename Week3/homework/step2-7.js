17a7620d32799b4e9abc951f93ea670bf607946d
Lisa Smeke <lisasmeke@gmail.com>
2022-06-16 12:45:09 +0200

    Explains functions' results

--- Week3/homework/step2-7.js
+++ Week3/homework/step2-7.js
@@ -6,8 +6,6 @@ function f1(val) {
   return val;
 }
 
-f1(x);
-
 console.log(x);
 
 const y = { x: 9 };
@@ -21,3 +19,7 @@ f2(y);
 console.log(y);
 
 // Add your explanation as a comment here
+// The variable in the first function is a number, while the variable in the second function is an object. 
+// f1 doesn't call the value of x, that's why when we console.log x, we get the value of that constant (9)
+// f2 calls the value of x and makes an operation, thus resulting in a new object "{x :10}"
+
