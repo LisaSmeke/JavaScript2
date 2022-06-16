30e631191c9f98f09d3b8ef1574299ecdf9cc903
Lisa Smeke <lisasmeke@gmail.com>
2022-06-16 13:04:09 +0200

    Creates function that adds 6 to any number

--- Week3/homework/step3.js
+++ Week3/homework/step3.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function createBase(base) {
-  // Replace this comment and the next line with your code
-  console.log(base);
+  function basePlusNum(num) {
+    return base + num;
+  }
+  return basePlusNum;
 }
 
 const addSix = createBase(6);
