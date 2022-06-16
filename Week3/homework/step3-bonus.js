5055ce00e638e56755117b7323ae5e10eee5cff8
Lisa Smeke <lisasmeke@gmail.com>
2022-06-16 13:30:17 +0200

    Writes function that removes duplicates from an array

--- Week3/homework/step3-bonus.js
+++ Week3/homework/step3-bonus.js
@@ -1,14 +1,30 @@
 'use strict';
 
-const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
-
 function makeUnique(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++){
+    if (newArr.indexOf(arr[i]) === -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 }
 
+const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
+const names = ['lisa', 'lisa', 'priscilla', 'tania', 'priscilla', 'nadia', 'nadia', 'nadia'];
+const numbers = [22, 22, 34, 30, 30, 30, 22, 15, 12, 29, 29, 28, 29];  
+
+
 const uniqueValues = makeUnique(values);
-console.log(uniqueValues);
+const uniqueNames = makeUnique(names);
+const uniqueNumbers = makeUnique(numbers);
+
+console.log(uniqueValues, uniqueNames, uniqueNumbers);
+
+
+// Another SHORTER option is: 
+// let newArr = [... new Set(arr)];
+// return newArr;
 
 // Do not change or remove anything below this line
 module.exports = makeUnique;
